d117bae86bbeac5327a9797acedf3b1670fa9028
Ronja Ö <tomoe_cchi@hotmail.com>
2022-01-20 16:45:07 +0100

    Snart i mål!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Console } = require("console");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -15,7 +16,21 @@ app.get("/", (req, res) => {
 });
 
 let connectedUsers = new Array();
+let cellsMarkedX = new Array();
+let cellsMarkedO = new Array();
+let allClickedCells = new Array();
 let whosTurnItIs = 0;
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+const allCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 // Anropas när någon ansluter till servern.
 io.on("connection", (socket) => {
@@ -24,32 +39,82 @@ io.on("connection", (socket) => {
 
     connectedUsers.push(socket.user);
 
-    console.log(`${socket.user} has connected.`);
+    console.log(`${socket.user} har anslutit till sidan.`);
 
-    io.emit("connected to room", connectedUsers);
+    io.emit("connected to room", connectedUsers, connectedUsers.length);
   });
 
   // ...
-  socket.on("cell was clicked", (targetedCellsCellIndex) => {
-    io.emit("turn change", targetedCellsCellIndex, whosTurnItIs, socket.id);
+  socket.on("cell was clicked", (targetedCellIndex) => {
+    let markToAdd;
 
-    if (whosTurnItIs == 0) {
-      whosTurnItIs = 1;
-    } else if (whosTurnItIs == 1) {
-      whosTurnItIs = 0;
+    allClickedCells.push(targetedCellIndex);
+
+    if (isItADraw(allClickedCells)) {
+      console.log("Spelet blev oavgjort!");
+
+      io.emit("itsADraw");
+    } else {
+      whosTurnItIs == 0 ? (markToAdd = "X") : (markToAdd = "O");
+
+      io.emit("turn change", targetedCellIndex, markToAdd, socket.id);
+
+      if (whosTurnItIs == 0) {
+        cellsMarkedX.push(targetedCellIndex);
+        whosTurnItIs = 1;
+      } else {
+        cellsMarkedO.push(targetedCellIndex);
+        whosTurnItIs = 0;
+      }
+
+      // Kontrollerar vinst!
+      if (didSomeoneWin(cellsMarkedX)) {
+        console.log("X vann spelet!");
+
+        io.emit("PlayerXWon", socket.user);
+      } else if (didSomeoneWin(cellsMarkedO)) {
+        console.log("O vann spelet!");
+
+        io.emit("PlayerOWon", socket.user);
+      }
     }
+  });
+
+  socket.on("restart game request", () => {
+    cellsMarkedX = [];
+    cellsMarkedO = [];
+    allClickedCells = [];
+    whosTurnItIs = 0;
+
+    console.log("Spelet startas om!");
+
+    io.emit("game restarted");
   });
 
   // Anropas när någon kopplar ifrån servern.
   socket.on("disconnect", () => {
-    console.log(`${socket.user} has disconnected.`);
+    console.log(`${socket.user} har kopplat från sidan.`);
 
     // Filtrerar arrayen och behåller alla som inte är den aktuella användaren.
     connectedUsers = connectedUsers.filter((item) => item !== socket.user);
 
     // Skickar array med alla anslutna användare till klienten.
-    io.emit("connected to room", connectedUsers);
+    io.emit("connected to room", connectedUsers, connectedUsers.length);
   });
+
+  function didSomeoneWin(arrayXsOrOs) {
+    return winningCombinations.some((combinations) => {
+      return combinations.every((winItem) => {
+        return arrayXsOrOs.includes(winItem);
+      });
+    });
+  }
+
+  function isItADraw(arrayClickedCells) {
+    return allCells.every((drawItem) => {
+      return arrayClickedCells.includes(drawItem);
+    });
+  }
 });
 
 server.listen(port, () => console.log(`Listening on *:${port}`));
--- public/js/clientscript.js
+++ public/js/clientscript.js
@@ -1,30 +1,42 @@
 window.onload = () => {
   const socket = io();
 
+  const usernameForm = document.getElementById("usernameForm");
   const cellElements = document.querySelectorAll("td");
+  const gameBoard = document.getElementById("gameBoard");
+  const finishedGameMessage = document.getElementById("finishedGameMessage");
+  const playAgainButton = document.getElementById("playAgainButton");
   let usersSocketID;
 
+  // Sparar användarens socket-ID i en variabel vid anslutning.
   socket.on("connect", () => {
     usersSocketID = socket.id;
   });
 
-  document.getElementById("usernameForm").addEventListener("submit", (evt) => {
+  // Lyssnar efter inlämning av användarnamnsformuläret (i framtiden även rums-ID).
+  usernameForm.addEventListener("submit", (evt) => {
+    // Hindrar sidan från att uppdateras.
     evt.preventDefault();
 
     const usernameInput = document.getElementById("inputUsername").value;
 
-    // Kontrollerar att användarnamnet endast innehåller bokstäver.
+    // Kontrollerar att användarnamnet endast innehåller bokstäver och är tillräckligt långt/kort.
     if (
       !/[^a-zA-ZåäöÅÄÖ]/.test(usernameInput) &&
       usernameInput.length >= 2 &&
       usernameInput.length <= 15
     ) {
+      // Skickar att {användarnamn} har anslutit (om användarnamnet godkänns) till servern.
       socket.emit("new user connected", usernameInput);
 
+      // Döljer formulär-div.
       formContent.style.display = "none";
 
-      // Börjar som "display: none" i style.css-filen.
-      mainContent.style.display = "block";
+      // Visar rums-div och spel-div. Börjar som "display: none" i style.css-filen.
+      roomContent.style.display = "block";
+      gameContent.style.display = "block";
+
+      // Varnar användaren ifall användarnamnet innehåller otillåtna tecken eller är för långt/kort.
     } else if (/[^a-zA-ZåäöÅÄÖ]/.test(usernameInput)) {
       alert(
         "Användarnamnet får endast innehålla tecknena A-Ö. Vänligen försök igen."
@@ -35,45 +47,96 @@ window.onload = () => {
       );
     }
 
+    // Återställer formulärets inmatningsfält.
     document.getElementById("inputUsername").value = "";
   });
 
-  // Tar emot array med alla användare anslutna (till det aktuella rummet) från servern och postar i webbläsaren.
-  socket.on("connected to room", (everyoneHere) => {
+  // Tar emot array med alla anslutna användares användarnamn från servern.
+  socket.on("connected to room", (everyoneHere, howManyAreHere) => {
     let connectedUsers = "";
 
     for (item in everyoneHere) {
       connectedUsers += "<li>" + everyoneHere[item] + "</li>";
     }
 
-    usersInRoom.innerHTML = connectedUsers;
+    // Postar arrayen i webbläsaren för alla användare.
+    document.getElementById("usersInRoom").innerHTML = connectedUsers;
+
+    /* Gör spelet oklickbart om antalet spelare är fler eller färre än två.
+    ATT GÖRA: Ändra så att max två användare kan vara anslutna samtidigt, annars kan spelare X bli spelare O när en tredje användare kopplar ifrån. */
+    if (howManyAreHere == 2) {
+      gameBoard.classList.remove("non-clickable");
+    } else if (howManyAreHere > 2) {
+      gameBoard.classList.add("non-clickable");
+
+      alert("Max två användare kan spela samtidigt.");
+    }
   });
 
+  // Lyssnar efter en klick-händelse för varje individuell table cell.
   cellElements.forEach((cell) => {
     cell.addEventListener("click", (cell) => {
       const targetCell = cell.target;
 
-      socket.emit("cell was clicked", targetCell.cellIndex);
+      // Gör spelet oklickbart för samtliga spelare (tills servern har svarat, se socket.on("turn change") här nedan).
+      gameBoard.classList.add("non-clickable");
 
-      document.getElementById("gameBoard").classList.add("non-clickable");
+      // Skickar information om vilken cell som precis klickades till servern.
+      socket.emit("cell was clicked", targetCell.cellIndex);
     });
   });
 
-  socket.on("turn change", (targetedCellIndex, currentTurn, activeSocket) => {
+  // Tar emot från servern.
+  socket.on("turn change", (targetedCellIndex, currentMark, activeSocket) => {
     const thisCell = document.getElementsByTagName("td")[targetedCellIndex];
 
-    if (currentTurn == 0) {
-      thisCell.innerHTML = "X";
-    } else if (currentTurn == 1) {
-      thisCell.innerHTML = "O";
-    }
+    thisCell.innerHTML = currentMark;
 
     thisCell.classList.add("non-clickable");
 
-    //if () {};
-
     if (activeSocket != usersSocketID) {
-      document.getElementById("gameBoard").classList.remove("non-clickable");
+      gameBoard.classList.remove("non-clickable");
     }
+  });
+
+  // Tar emot från servern.
+  socket.on("itsADraw", () => {
+    finishedGameMessage.innerText = "Spelet är oavgjort!";
+
+    gameContent.style.display = "none";
+    finishedContent.style.display = "block";
+  });
+
+  // Tar emot från servern.
+  socket.on("PlayerXWon", (usernamePlayerX) => {
+    finishedGameMessage.innerText = `${usernamePlayerX} är vinnaren!`;
+
+    gameContent.style.display = "none";
+    finishedContent.style.display = "block";
+  });
+
+  // Tar emot från servern.
+  socket.on("PlayerOWon", (usernamePlayerO) => {
+    finishedGameMessage.innerText = `${usernamePlayerO} är vinnaren!`;
+
+    gameContent.style.display = "none";
+    finishedContent.style.display = "block";
+  });
+
+  playAgainButton.addEventListener("click", () => {
+    // Skickar till servern.
+    socket.emit("restart game request");
+  });
+
+  // Tar emot från servern.
+  socket.on("game restarted", () => {
+    cellElements.forEach((cell) => {
+      cell.classList.remove("non-clickable");
+      cell.innerHTML = "";
+    });
+
+    finishedGameMessage.innerText = "";
+    finishedContent.style.display = "none";
+    gameContent.style.display = "block";
   });
 };
